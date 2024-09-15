534bd0ec62a1ed4a74c90b43ceff913fc0b7368e
lucashoriisant <luck201211@hotmail.com>
2024-09-14 22:45:00 -0300

    Realizado a inclusão dos TODO's, na classe ContaTerminal.java

--- conta-java/src/ContaTerminal.java
+++ conta-java/src/ContaTerminal.java
@@ -1,5 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    //TODO: Conhecer e importar a classe Scanner
+
+    //TODO: Exibir as mensagens para o nosso usuário
+
+    //TODO: Obter por meio do scanner os valores digitados no terminal
+
+    //TODO: Exibir a mensagem da conta criada
+
     }
 }
