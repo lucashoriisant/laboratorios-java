a78659b10bad4c762a77bd1c624b9012300702c1
lucashoriisant <luck201211@hotmail.com>
2024-09-16 06:24:38 -0300

    Inclusão dos TODO's

--- conta-java/src/ContaTerminal.java
+++ conta-java/src/ContaTerminal.java
@@ -1,13 +1,32 @@
-public class ContaTerminal {
+import java.util.Locale;
+import java.util.Scanner;
+
+    public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
+        int numero = 0;
+        String agencia;
+        String nomeCliente;
+        double saldo = 0;
+
+        //TODO: Conhecer e importar a classe Scanner
+
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //TODO: Exibir as mensagens para o nosso usuário  //TODO: Obter por meio do scanner os valores digitados no terminal
+
+            System.out.println("Por favor, digite o seu nome!");
+            nomeCliente = scanner.next();
+
+            System.out.println("Por favor, digite o numero da sua conta!");
+            numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o numero da Agencia!");
+            agencia = scanner.next();
+
+        //TODO: Exibir a mensagem da conta criada
+            System.out.println("Ola " + nomeCliente +  ", obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + " , conta " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque");
     
-    //TODO: Conhecer e importar a classe Scanner
-
-    //TODO: Exibir as mensagens para o nosso usuário
-
-    //TODO: Obter por meio do scanner os valores digitados no terminal
-
-    //TODO: Exibir a mensagem da conta criada
-
     }
+
 }
